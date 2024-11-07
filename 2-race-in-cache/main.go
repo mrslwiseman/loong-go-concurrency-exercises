db28b154552f122b81d6a22bd293a9075e3e28f4
Matt Wiseman <mrslwiseman@gmail.com>
2024-11-07 20:41:32 +1030

    solution-2

--- 2-race-in-cache/main.go
+++ 2-race-in-cache/main.go
@@ -10,6 +10,7 @@ package main
 
 import (
 	"container/list"
+	"sync"
 	"testing"
 )
 
@@ -29,6 +30,7 @@ type page struct {
 
 // KeyStoreCache is a LRU cache for string key-value pairs
 type KeyStoreCache struct {
+	sync.Mutex
 	cache map[string]*list.Element
 	pages list.List
 	load  func(string) string
@@ -44,6 +46,9 @@ func New(load KeyStoreCacheLoader) *KeyStoreCache {
 
 // Get gets the key from cache, loads it from the source if needed
 func (k *KeyStoreCache) Get(key string) string {
+	k.Lock()
+	defer k.Unlock()
+	// return from cache if present
 	if e, ok := k.cache[key]; ok {
 		k.pages.MoveToFront(e)
 		return e.Value.(page).Value
@@ -65,6 +70,7 @@ func (k *KeyStoreCache) Get(key string) string {
 
 // Loader implements KeyStoreLoader
 type Loader struct {
+	sync.Mutex
 	DB *MockDB
 }
 
